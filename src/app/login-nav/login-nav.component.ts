3f6642a6c178abf569a4721314e6c5f412406331
Hrishi <hrishikesh@iglulabs.com>
2020-11-24 18:16:26 +0530

    Fromat code

--- src/app/login-nav/login-nav.component.ts
+++ src/app/login-nav/login-nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 declare var $
 @Component({
@@ -10,40 +10,39 @@ declare var $
 export class LoginNavComponent implements OnInit {
   navbarOpen = false;
   selectedItem = null;
- // dropdownVal = 'Vessels';
+  // dropdownVal = 'Vessels';
   constructor() { }
   name: string;
   position: number;
   weight: number;
   symbol: string;
   gender
-malecount=0;
-femaleCount=0;
-   ELEMENT_DATA= [
-    {name: 'Hrishikesh', birthDate: "12-12-2020", lang: 'Marathi',gender:'Male'},
-    {name: 'Mahesh', birthDate: "10-11-2020", lang: 'Hindi',gender:'Male'},
-    {name: 'Ankita', birthDate: "11-12-2020", lang: 'Enlish',gender:'Female'},
+  malecount = 0;
+  femaleCount = 0;
+  ELEMENT_DATA = [
+    { name: 'Hrishikesh', birthDate: "12-12-2020", lang: 'Marathi', gender: 'Male' },
+    { name: 'Mahesh', birthDate: "10-11-2020", lang: 'Hindi', gender: 'Male' },
+    { name: 'Ankita', birthDate: "11-12-2020", lang: 'Enlish', gender: 'Female' },
 
   ];
   displayedColumns: string[] = ['name', 'birthDate', 'lang', 'gender'];
   dataSource = this.ELEMENT_DATA;
-  rebels = this.dataSource.map( (item)=> {
-    if(item.gender == "Male")
-    {
-      this.malecount ++
+  rebels = this.dataSource.map((item) => {
+    if (item.gender == "Male") {
+      this.malecount++
     }
-    else{
-      this.femaleCount ++
+    else {
+      this.femaleCount++
     }
   });
-  userLenth=this.dataSource.length *10
+  userLenth = this.dataSource.length * 10
   toppings = [];
 
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  LanguagesList: string[] = ['Marathi', 'Hindi', 'English'];
   valuess
   topping
   ngOnInit() {
- 
+
   }
 
   toggleNavbar() {
